c61a75d958829d7f3e191d8e6144b77de99adb62
Dimitar Dimitrov <daghostman.dimitrov@gmail.com>
2016-08-05 13:03:00 +0100

    Added few tests to check for the configuration

--- tests/Helpers/MarkdownTest.php
+++ tests/Helpers/MarkdownTest.php
@@ -24,4 +24,38 @@ class MarkdownTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertSame('<p><strong>test</strong></p>', call_user_func($this->markdown, '**test**'));
     }
+
+    public function testDefaultOptionsFunctionality()
+    {
+        $this->assertSame(
+            '<p><strong>test</strong></p>',
+            call_user_func($this->markdown, "**test**\n\n\n")
+        );
+        $this->assertSame(
+            '<p><a href="https://google.com">https://google.com</a></p>',
+            call_user_func($this->markdown, "https://google.com")
+        );
+        $this->assertSame(
+            '<p>&lt;script&gt;alert(&quot;hello&quot;);&lt;/script&gt;</p>',
+            call_user_func($this->markdown, '<script>alert("hello");</script>')
+        );
+    }
+
+    public function testWithEscapeDisabled()
+    {
+        $md = new Markdown(['escape' => false]);
+        $this->assertSame(
+            '<p><strong>test</strong></p>',
+            call_user_func($md, '<strong>test</strong>')
+        );
+    }
+
+    public function testWithBreaksEnabled()
+    {
+        $md = new Markdown(['breaks' => true]);
+        $this->assertSame(
+            "<p><strong>test</strong><br />\n<em>Yes</em></p>",
+            call_user_func($md, "**test**\n*Yes*")
+        );
+    }
 }
